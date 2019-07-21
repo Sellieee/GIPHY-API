9c85afb764abd109cab6e097d552a4924ccbd1a2
Sellieee <sellieee@hotmail.com>
2019-07-21 22:12:59 +1000

    changed topic due to lack of results for some cartoon names

--- javascript/app.js
+++ javascript/app.js
@@ -1,27 +1,50 @@
 // Array of cartoons which will be the buttons.
-var cartoon = ["Pokémon", "Snoopy", "Digimon", "Bleach", "Hamtaro", "Hello Kitty", "Doraemon"];
+var games = ["Stardew Valley", "Overcooked 2", "Skyrim", "Don't Starve", "Tomb Raider"];
+
+function showGame() {
+    var title = $(this).attr("data-name");
+    // API URL with API and query, limited to 10 searches
+    var queryURL = "api.giphy.com/v1/gifs/search?q=" + title + "&apikey=t864AJwF6m3Ehbb934dGP3WyLshTk7yA&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.data;
+        for (var x = 0; x < results.length; x++) {
+            var gameDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[x].rating);
+            var image = $("<img>");
+            gameDiv.append(p);
+            gameDiv.append(image);
+            console.log(response);
+        }
+    })
+}
 
 // Display cartoon buttons
 function renderButtons() {
     $(".giphy-button").empty();
-    for (var i = 0; i < cartoon.length; i++) {
-        var button = $("<button>" + cartoon[i] + "</button>");
+    // Looping through cartoon array
+    for (var i = 0; i < games.length; i++) {
+        var a = $("<button>");
+        a.addClass("games");
+        a.attr("data-name", games[i]);
+        a.text(games[i]);
         button.appendTo(".giphy-button");
     }
 }
-
-
-$("#add-cartoon").click(function (event) {
+// Display movie data
+$("#add-game").click(function (event) {
     event.preventDefault();
     var input = "";
-    console.log($("#cartoon-input").val());
-    input = $("#cartoon-input").val();
-    cartoon.push(input);
-    console.log(cartoon);
+    console.log($("#game-input").val());
+    input = $("#game-input").val();
+    games.push(input);
+    console.log(games);
     renderButtons();
 })
-
-renderButtons();
 
 
 // On click - page takes 10 static gifs from GIPHY API
