64c9ef6a56bd9653d2cba5bfa773efd789cff6f6
Sellieee <sellieee@hotmail.com>
2019-07-22 19:37:02 +1000

    fixed minor issues, js still not showing

--- javascript/app.js
+++ javascript/app.js
@@ -1,32 +1,32 @@
-// Array of cartoons which will be the buttons.
+// Array of games which will be the buttons.
 var games = ["Stardew Valley", "Overcooked 2", "Skyrim", "Don't Starve", "Tomb Raider"];
 
-function showGame() {
+function displayGameinfo() {
     var title = $(this).attr("data-name");
     // API URL with API and query, limited to 10 searches
     var queryURL = "api.giphy.com/v1/gifs/search?q=" + title + "&apikey=t864AJwF6m3Ehbb934dGP3WyLshTk7yA&limit=10";
+};
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+    var results = response.data;
+    for (var x = 0; x < results.length; x++) {
+        var gameDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + results[x].rating);
+        var image = $("<img>");
+        gameDiv.append(p).append(image);
         console.log(response);
-        var results = response.data;
-        for (var x = 0; x < results.length; x++) {
-            var gameDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[x].rating);
-            var image = $("<img>");
-            gameDiv.append(p).append(image);
-            console.log(response);
-            $("#images").prepend(gameDiv);
-        }
-    })
-}
+        $("#images").prepend(gameDiv);
+    }
+})
 
-// Display cartoon buttons
+// Display game buttons
 function renderButtons() {
     $(".giphy-button").empty();
-    // Looping through cartoon array
+    // Looping through game array
     for (var i = 0; i < games.length; i++) {
         var a = $("<button>");
         a.addClass("games");
@@ -35,7 +35,7 @@ function renderButtons() {
         a.appendTo(".giphy-button");
     }
 }
-// Display movie data
+// Display game data
 $("#add-game").click(function (event) {
     event.preventDefault();
     var input = "";
