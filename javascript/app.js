06f6985633261357e1d8ba2d05fcc6d9271926fd
Sellieee <sellieee@hotmail.com>
2019-07-22 19:51:53 +1000

    updated js

--- javascript/app.js
+++ javascript/app.js
@@ -1,28 +1,6 @@
 // Array of games which will be the buttons.
 var games = ["Stardew Valley", "Overcooked 2", "Skyrim", "Don't Starve", "Tomb Raider"];
 
-function displayGameinfo() {
-    var title = $(this).attr("data-name");
-    // API URL with API and query, limited to 10 searches
-    var queryURL = "api.giphy.com/v1/gifs/search?q=" + title + "&apikey=t864AJwF6m3Ehbb934dGP3WyLshTk7yA&limit=10";
-};
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    var results = response.data;
-    for (var x = 0; x < results.length; x++) {
-        var gameDiv = $("<div>");
-        var p = $("<p>").text("Rating: " + results[x].rating);
-        var image = $("<img>");
-        gameDiv.append(p).append(image);
-        console.log(response);
-        $("#images").prepend(gameDiv);
-    }
-})
-
 // Display game buttons
 function renderButtons() {
     $(".giphy-button").empty();
@@ -35,6 +13,28 @@ function renderButtons() {
         a.appendTo(".giphy-button");
     }
 }
+
+function displayGameinfo() {
+    var title = $(this).attr("data-name");
+    // API URL with API and query, limited to 10 searches
+    var queryURL = "api.giphy.com/v1/gifs/search?q=" + title + "&apikey=t864AJwF6m3Ehbb934dGP3WyLshTk7yA&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.data;
+        for (var x = 0; x < results.length; x++) {
+            var gameDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[x].rating);
+            var image = $("<img>");
+            gameDiv.append(p).append(image);
+            console.log(response);
+            $("#images").prepend(gameDiv);
+        }
+    })
+};
+
 // Display game data
 $("#add-game").click(function (event) {
     event.preventDefault();
@@ -46,6 +46,8 @@ $("#add-game").click(function (event) {
     renderButtons();
 })
 
+$(document).click(".games", displayGameinfo);
+renderButtons();
 
 // On click - page takes 10 static gifs from GIPHY API
 // On click of images, the gifs animate, and on click again = back to static
